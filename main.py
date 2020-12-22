08382d7de7ec9cb1c32c88a250fcc400ff623352
inbruk <35333872+inbruk@users.noreply.github.com>
2020-12-22 18:19:24 +0300

    ................ю

--- main.py
+++ main.py
@@ -111,3 +111,99 @@ display(sample)
 sample2 = sample.copy(True)
 sample2.City = sample2.City.astype(str).apply(lambda x: x.lower())
 display(sample2)
+
+print('-------------------------------------------------------')
+
+sample = pd.read_csv("sample.csv")
+sample2 = sample.copy(True)
+
+def profession_code(s):
+    if s == "Рабочий":
+        return 0
+    elif s == "Менеджер":
+        return 1
+    return 2
+
+sample2.Profession = sample2.Profession.astype(str).apply( profession_code )
+display(sample2)
+
+print('-------------------------------------------------------')
+
+sample = pd.read_csv("sample.csv")
+
+def age_category(a):
+    if a < 23:
+        return "молодой"
+    elif 23 <= a <= 35 :
+        return "средний"
+    return "зрелый"
+
+sample['Age_category']  = sample.Age.apply( age_category )
+
+print('-------------------------------------------------------')
+
+log = pd.read_csv('log.csv', header=None)
+log.columns = ['user_id','time','bet','win']
+display(log)
+
+def prepare_user_id(s):
+    arr_str = s.split(' - ')
+    if len(arr_str)>1:
+        return arr_str[1]
+    else:
+        return ''
+
+log['user_id'] = log.user_id.apply( prepare_user_id )
+display(log)
+
+print('-------------------------------------------------------')
+
+log = pd.read_csv("log.csv",header=None)
+log.columns = ['user_id','time','bet','win']
+t = log.time[0]
+t = t.replace('[','')
+display(t)
+
+print('-------------------------------------------------------')
+
+log = pd.read_csv('log.csv', header=None)
+log.columns = ['user_id','time','bet','win']
+display(log)
+
+
+def prepare_time(s):
+    res = s.replace('[','')
+    #res = res.replace('nan','')
+    return res
+
+log['time'] = log.time.astype(str).apply( prepare_time )
+display(log)
+
+print('-------------------------------------------------------')
+
+log = pd.read_csv('log.csv', header=None)
+log.columns = ['user_id','time','bet','win']
+
+log = log[ log.user_id!='#error' ]
+
+
+def prepare_user_id(s):
+    arr_str = s.split(' - ')
+    if len(arr_str)>1:
+        return arr_str[1]
+    else:
+        return ''
+log['user_id'] = log.user_id.apply( prepare_user_id )
+
+
+def prepare_time(s):
+    res = s.replace('[','')
+    return res
+
+
+log['time'] = log.time.astype(str).apply( prepare_time )
+display(log)
+
+
+
+
