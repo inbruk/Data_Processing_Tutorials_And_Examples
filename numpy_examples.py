efea168a957a455e8d4aa514b0531ab181cfe0bf
inbruk <35333872+inbruk@users.noreply.github.com>
2021-06-19 03:45:15 +0300

    ................ю.d........

--- numpy_examples.py
+++ numpy_examples.py
@@ -157,6 +157,90 @@ res_arr2 = res_arr1[99]
 res = res_arr2.sum()
 print(round(res,2))
 
-print('B7.3.1.1 ------------------------------------------------')
+students = np.array([
+    [1,	 135, 34, 4],
+    [2,	 160, 43, 5],
+    [3,  163, 40, 4.3],
+    [4,  147, 44, 5],
+    [5,  138, 41, 4.7],
+    [6,  149, 54, 3.9],
+    [7,  136, 39, 4.2],
+    [8,  154, 48, 4.9],
+    [9,  137, 35, 3.7],
+    [10, 165, 60, 4.6]
+])
 
-arr3 = np.array([[1,2,3],[4,5,6],[7,8,9]])
+studT = students.T
+print(studT)
+
+print('B7.3.1.1 ------------------------------------------------')
+print(np.median(students[:, 2]))
+
+print('B7.3.1.2 ------------------------------------------------')
+print(round(np.mean(students[:, 2]) - np.median(students[:, 2]), 1))
+
+print('B7.3.1.3 ------------------------------------------------')
+corr1 = np.corrcoef(students[:,1], students[:,2])
+corr2 = np.corrcoef(students[:,1], students[:,3])
+corr3 = np.corrcoef(students[:,2], students[:,3])
+print(corr1)
+print(corr2)
+print(corr3)
+
+print('B7.3.1.4 ------------------------------------------------')
+std = np.std(students[:, 3])
+print(std)
+
+print('B7.3.1.5 ------------------------------------------------')
+std = np.std(students[:, 2])
+print(std*std)
+
+print('B7.4.1.4 ------------------------------------------------')
+my_array = np.array([[1,2,3,4,5],
+                     [6,7,8,9,10],
+                     [11,12,13,14,15],
+                     [16,17,18,19,20],
+                     [21,22,23,24,25]])
+
+new_array = my_array[1:4, 1:4]
+print(new_array)
+
+print('B7.4.1.5 ------------------------------------------------')
+my_sin = np.sin(my_array)
+res = round(np.sum(my_sin), 3)
+print(res)
+
+print('B7.4.1.6 ------------------------------------------------')
+for i in range(1,4):
+    for j in range(1,4):
+        my_sin[i][j] = 1
+print(my_sin)
+
+res = round(np.sum(my_sin), 3)
+print(res)
+
+print('B7.4.1.7 ------------------------------------------------')
+new_arr = my_sin[0:5, 0:4]
+print(new_arr)
+
+res_arr4 = new_arr.reshape((10, 2))
+print(res_arr4)
+
+res_arr5 = res_arr4[0:11, 0:1]
+print(res_arr5)
+
+res = round(np.sum(res_arr5), 3)
+print(res)
+
+print(' ------------------------------------------------')
+
+bigdata = np.array()
+for x in range(100, 1001):
+    if x % 2 == 0:
+        bigdata = np.append([x])
+print(bigdata)
+
+print('B7.4.1.7 ------------------------------------------------')
+
+res = np.median(bigdata)
+print(res)
