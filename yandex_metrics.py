2b61656da76a55bdb7cefb35090af0873fa34f19
inbruk <35333872+inbruk@users.noreply.github.com>
2021-08-20 15:53:08 +0300

    ................ю.d....................

---
+++ yandex_metrics.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import operator
+import json
+from pprint import pprint
+import xml.etree.ElementTree as ET
+import xmljson
+import requests
+from bs4 import BeautifulSoup
+from pprint import pprint
+
+
