ad2f9df0f15970edacc222b1225bb90a6d71989c
inbruk <35333872+inbruk@users.noreply.github.com>
2021-11-15 16:34:52 +0300

    ................ю.d...........................

--- examination.py
+++ examination.py
@@ -15,6 +15,8 @@ import re
 
 revenue_data = pd.read_csv("revenue_data.csv", sep='\t', decimal=',')
 pprint(revenue_data)
+pprint('------------------------------------------------------------------------------------')
+
 
 token = 'AQAAAABXjzIIAAdSVYTRaTUWbUtzlhqF3lZc5uM'
 headers = {'Authorization': 'OAuth ' + token}
@@ -29,3 +31,32 @@ response = requests.get('https://api-metrika.yandex.net/stat/v1/data', params=pa
 
 metrika_data = response.json()
 pprint(metrika_data)
+print('------------------------------------------------------------------------------------')
+
+
+df = pd.DataFrame(columns=['deviceCategory', 'mobilePhone', 'mobilePhoneModel', 'usersCount'])
+#pprint(df)
+
+# lst = metrika_data['data']
+# pprint(lst)
+
+curr_item = metrika_data['data'][0]
+pprint(curr_item)
+print('------------------------------------------------------------------------------------')
+
+curr_dims = curr_item['dimensions']
+curr_deviceCategory = curr_dims[0]['name']
+curr_mobilePhone = curr_dims[1]['name']
+curr_mobilePhoneModel = curr_dims[2]['name']
+pprint(str(curr_deviceCategory) + ' ' + str(curr_mobilePhone) + ' ' + str(curr_mobilePhoneModel))
+print('------------------------------------------------------------------------------------')
+
+curr_met = curr_item['metrics']
+curr_usr_count = curr_met[0]
+pprint(str(curr_usr_count))
+print('------------------------------------------------------------------------------------')
+
+# res_lst = list(filter(lambda x: 'Python' in x['dimensions'][0]['name'], metrika_data))
+# pprint(res_lst)
+
+
