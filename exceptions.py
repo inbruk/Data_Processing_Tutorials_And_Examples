c4c3fb1e5bd1844f2f915055b36c04b3d0412b6e
inbruk <inbruk@mail.ru>
2024-05-19 01:46:11 +0300

    ................ю.d..............................1

--- exceptions.py
+++ exceptions.py
@@ -1,14 +1,26 @@
-import numpy as np
-import pandas as pd
-import operator
-import json
-from pprint import pprint
-import xml.etree.ElementTree as ET
-import xmljson
-import requests
-from bs4 import BeautifulSoup
-from pprint import pprint
-import datetime
-import time
-import schedule
-import re
+# import numpy as np
+# import pandas as pd
+# import operator
+# import json
+# from pprint import pprint
+# import xmljson
+# import requests
+# from bs4 import BeautifulSoup
+# from pprint import pprint
+# import datetime
+# import time
+# import schedule
+# import re
+from IPython.core.display import display
+
+
+def check_server(mode):
+    if mode == "memory":
+        print("Memory is ok")
+    elif mode == "connection":
+        print("Connection is ok")
+    else:
+        raise ValueError()
+
+
+check_server("memory")
